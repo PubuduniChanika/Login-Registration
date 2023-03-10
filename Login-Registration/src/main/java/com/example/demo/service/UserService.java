646bfa60971e983977694ffc2a14ae10f4aaab85
Pubudini <Pubudini@Pubudini-PC>
2023-03-10 11:49:21 +0530

    First Commit

---
+++ Login-Registration/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.dto.UserDto;
+
+import com.example.demo.model.User;
+
+public interface UserService {
+    void saveUser(UserDto userDto);
+
+    User findUserByEmail(String email);
+}
